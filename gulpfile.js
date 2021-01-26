3e5d25bed937a4d048ebccace67f2754d71556fb
guypystin <51515987+guypystin@users.noreply.github.com>
2021-01-26 21:06:37 +0300

    header без адаптации под смартфоны

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    browserSync = require('browser-sync');
+    autoprefixer = require('gulp-autoprefixer');
+
+
+ gulp.task('scripts', function() {
+    return gulp.src(['app/js/common.js', 'app/libs/**/*.js'])
+    .pipe(browserSync.reload({ stream: true }))
+});
+gulp.task('code', function() {
+	return gulp.src('app/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+gulp.task('sass', function() {
+    return gulp.src('app/sass/**/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('app/css'))
+      .pipe(browserSync.reload({stream: true}))
+});
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false
+    });
+});
+gulp.task('watch', function() {
+    gulp.watch('app/sass/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('app/*.html', gulp.parallel('code')); // Наблюдение за HTML файлами в корне проекта
+	gulp.watch(['app/js/common.js', 'app/libs/**/*.js'], gulp.parallel('scripts')); // Наблюдение за главным JS файлом и за библиотеками
+});
+gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
